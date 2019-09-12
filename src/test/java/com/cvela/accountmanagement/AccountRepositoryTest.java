22b7e33989337ad6a5b686526bb40206051dc657
Carl Allan Vela <carlallanvela@gmail.com>
2019-09-12 13:29:15 +0800

    Simple Unit Test for Account Repository.
    Simple Unit Test for Account Repository.

--- src/test/java/com/cvela/accountmanagement/AccountRepositoryTest.java
+++ src/test/java/com/cvela/accountmanagement/AccountRepositoryTest.java
@@ -3,38 +3,56 @@ package com.cvela.accountmanagement;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.Date;
+import java.util.List;
 import java.util.Optional;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.cvela.accountmanagement.account.Account;
 import com.cvela.accountmanagement.repository.AccountRepository;
+import com.cvela.accountmanagement.repository.TransactionRepository;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
 public class AccountRepositoryTest {
-	@Autowired
-	private TestEntityManager entityManager;
 	
 	@Autowired
-	private AccountRepository repository;
+	private AccountRepository accountRepository;
+	
+	@Autowired
+	private TransactionRepository transactionRepository;
 	
 	@Test
-	public void testFindByName() {
-		Account account = new Account(100, "Account 1", new Date());
-		account.setTransactions(null);
-		entityManager.persist(account);
-		entityManager.flush();
-		
-		Optional<Account> accountFound = repository.findById(1);
-		
-		assertThat(accountFound.get().getId() == account.getId());
+	public void testFindAll() {
+		List<Account> allAccounts = accountRepository.findAll();
+		// 3 initial accounts
+		assertThat(allAccounts.size() == 3);
+	}
+	
+	@Test
+	public void testFindOne() {
+		Optional<Account> account = accountRepository.findById(10001);
+		assertThat(account.get().getName() == "Account 1");
+	}
+	
+	@Test
+	public void testSave() {
+		Account newAccount = accountRepository.save(new Account(1, "New Account", new Date()));
+		Optional<Account> searchedAccount = accountRepository.findById(1);
+		assertThat(newAccount.getId() == searchedAccount.get().getId());
+	}
+	
+	@Test
+	public void testDelete() {
+		// Delete all transactions first
+		transactionRepository.deleteAll();
+		accountRepository.deleteAll();
+		List<Account> allAccounts = accountRepository.findAll();
+		assertThat(allAccounts.size() == 0);
 		
 	}
-
 }
